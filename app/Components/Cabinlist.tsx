2ad53c1cd22ebe1b5c0f6117c1ef01f95a989ee5
alvinChristianto <alvinchristianto93@gmail.com>
2023-08-11 13:56:12 +0700

    edit text

--- app/Components/Cabinlist.tsx
+++ app/Components/Cabinlist.tsx
@@ -39,7 +39,7 @@ function CabinList(props: any) {
           <div className="flex justify-center">
             <div className="max-w-[700px] text-center mb-5">
               <h2 className="mb-6 mt-6 text-xl font-primary font-semibold text-slate-900">
-                Berikut cabang <br />
+                Cabang <br />
                 <span className="text-primary font-primary    text-3xl font-bold">
                   The Cabin Hotel
                 </span>
